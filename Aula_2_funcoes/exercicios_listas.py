4ccff4efe69662732e0201973f05a76ea60cf411
yasminllz <yasminjhocelinm@gmail.com>
2025-10-11 17:53:39 -0300

    adicao de pequenas mudancas

--- Aula_2_funcoes/exercicios_listas.py
+++ Aula_2_funcoes/exercicios_listas.py
@@ -3,7 +3,7 @@
 
 # 0 escreva um programa que use a função range() para gerar os pares de 2 a 20 e, eseguida imprima cada numero 
 
-pares = list(range(0,21,2))
+pares = list(range(2,21,2))
 print(pares)
 
 # 1 crie uma lista chamada numeros que contenha os numeros que contenha os numeros inteiros de 1 a 10 e imprima-os na tela 
@@ -24,3 +24,16 @@ print(numeros)
 
 numeros.remove(5)
 print(numeros)
+
+# **Exercício 5:** Crie uma lista chamada carros contendo três nomes de carros diferentes. Em seguida, concatene essa lista com a lista 
+# `numeros` e imprima o resultado.
+
+
+carros = ['Jeep','BMW','Ferrarri']
+
+
+# numeros.extend([carros])
+
+
+# numeros += (carros)
+print(numeros, carros)
